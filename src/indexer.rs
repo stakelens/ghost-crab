45752c2c574b18362a12fadf71e09299f4f39b7e
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-06-21 09:04:38 -0400

    Refactor process_logs

--- src/indexer.rs
+++ src/indexer.rs
@@ -15,59 +15,19 @@ pub struct Context {
 }
 
 #[async_trait]
-pub trait Handleable {
+pub trait Handler {
     async fn handle(&self, params: Context);
-    fn get_data_source(&self) -> String;
+    fn get_source(&self) -> String;
     fn get_event_signature(&self) -> String;
-}
-
-pub struct ProcessLogsParams<'a> {
-    pub filter: Filter,
-    pub handler: Arc<Box<(dyn Handleable + Send + Sync)>>,
-    pub provider: &'a RootProvider<Http<Client>>,
-    pub conn: Arc<Mutex<PgConnection>>,
-}
-
-pub async fn process_logs_in_range(
-    ProcessLogsParams {
-        filter,
-        handler,
-        provider,
-        conn,
-    }: ProcessLogsParams<'_>,
-) {
-    let logs = provider.get_logs(&filter).await.unwrap();
-
-    let handlers = logs
-        .into_iter()
-        .map(|log| {
-            let conn = Arc::clone(&conn);
-            let handler = Arc::clone(&handler);
-            let provider = provider.clone();
-
-            tokio::spawn(async move {
-                handler
-                    .handle(Context {
-                        log,
-                        provider,
-                        conn,
-                    })
-                    .await;
-            })
-        })
-        .collect::<Vec<_>>();
-
-    for handle in handlers {
-        handle.await.unwrap();
-    }
+    fn is_template(&self) -> bool;
 }
 
 pub struct ProcessLogs {
     pub start_block: u64,
     pub step: u64,
     pub address: String,
-    pub handler: Box<(dyn Handleable + Send + Sync)>,
-    pub provider: Arc<RootProvider<Http<Client>>>,
+    pub handler: Box<(dyn Handler + Send + Sync)>,
+    pub provider: RootProvider<Http<Client>>,
     pub conn: Arc<Mutex<PgConnection>>,
 }
 
@@ -90,14 +50,14 @@ pub async fn process_log(
         let mut end_block = current_block + step;
         let latest_block = provider.get_block_number().await.unwrap();
 
-        if current_block == end_block {
+        if end_block > latest_block {
+            end_block = latest_block;
+        }
+
+        if current_block >= end_block {
             println!("Reached latest block: {}", current_block);
             tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
             continue;
-        }
-
-        if end_block > latest_block {
-            end_block = latest_block;
         }
 
         println!("Processing logs from {} to {}", current_block, end_block);
@@ -108,13 +68,30 @@ pub async fn process_log(
             .from_block(current_block)
             .to_block(end_block);
 
-        process_logs_in_range(ProcessLogsParams {
-            filter,
-            handler: Arc::clone(&handler),
-            provider: &provider,
-            conn: conn.clone(),
-        })
-        .await;
+        let logs = provider.get_logs(&filter).await.unwrap();
+
+        let handlers = logs
+            .into_iter()
+            .map(|log| {
+                let conn = Arc::clone(&conn);
+                let handler = Arc::clone(&handler);
+                let provider = provider.clone();
+
+                tokio::spawn(async move {
+                    handler
+                        .handle(Context {
+                            log,
+                            provider,
+                            conn,
+                        })
+                        .await;
+                })
+            })
+            .collect::<Vec<_>>();
+
+        for handle in handlers {
+            handle.await.unwrap();
+        }
 
         current_block = end_block;
     }
@@ -124,22 +101,22 @@ pub struct DataSourceConfig {
     pub start_block: u64,
     pub step: u64,
     pub address: String,
-    pub handler: Box<(dyn Handleable + Send + Sync)>,
+    pub handler: Box<(dyn Handler + Send + Sync)>,
     pub rpc_url: String,
 }
 
-pub struct Config {
-    pub db_url: String,
+pub struct RunInput {
+    pub database: String,
     pub data_sources: Vec<DataSourceConfig>,
 }
 
-pub async fn run(config: Config) {
-    let mut ingesters = cache::manager::RPCManager::new(config.db_url.clone());
-    let conn = establish_connection(config.db_url);
+pub async fn run(input: RunInput) {
+    let mut rpc_manager = cache::manager::RPCManager::new(input.database.clone());
+    let conn = establish_connection(input.database);
 
     let conn = Arc::new(Mutex::new(conn));
 
-    let handlers = config
+    let handlers = input
         .data_sources
         .into_iter()
         .map(|data_source| ProcessLogs {
@@ -147,7 +124,7 @@ pub async fn run(config: Config) {
             step: data_source.step,
             address: data_source.address.clone(),
             handler: data_source.handler,
-            provider: Arc::new(ingesters.get(data_source.rpc_url)),
+            provider: rpc_manager.get(data_source.rpc_url),
             conn: Arc::clone(&conn),
         })
         .collect::<Vec<_>>();
