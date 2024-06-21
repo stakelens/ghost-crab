cd4dca7b49c8a45d7fc2e916384f2113e870459b
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-06-21 09:00:58 -0400

    Now we can  make multiple instances of RPCManager because CURRENT_PORT is global

--- src/cache/manager.rs
+++ src/cache/manager.rs
@@ -1,21 +1,24 @@
 use alloy::providers::ProviderBuilder;
 use alloy::providers::RootProvider;
 use alloy::transports::http::{Client, Http};
+use once_cell::sync::Lazy;
 use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 use super::rpc_proxy::RpcWithCache;
 
 pub struct RPCManager {
     db_url: String,
-    current_port: u16,
     rpcs: HashMap<String, RootProvider<Http<Client>>>,
 }
+
+static CURRENT_PORT: Lazy<Arc<Mutex<u16>>> = Lazy::new(|| Arc::new(Mutex::new(3000)));
 
 impl RPCManager {
     pub fn new(db_url: String) -> Self {
         RPCManager {
             db_url: db_url,
-            current_port: 3000,
             rpcs: HashMap::new(),
         }
     }
@@ -28,8 +31,9 @@ impl RPCManager {
                 return value.clone();
             }
             None => {
+                let mut current_port = CURRENT_PORT.lock().unwrap();
                 let provider = ProviderBuilder::new().on_http(
-                    format!("http://localhost:{}", self.current_port)
+                    format!("http://localhost:{}", current_port)
                         .parse()
                         .unwrap(),
                 );
@@ -38,13 +42,13 @@ impl RPCManager {
 
                 // Start the Ingester service
                 let rpc_with_cache =
-                    RpcWithCache::new(self.db_url.clone(), rpc_url.clone(), self.current_port);
+                    RpcWithCache::new(self.db_url.clone(), rpc_url.clone(), *current_port);
 
                 tokio::spawn(async move {
                     rpc_with_cache.run().await;
                 });
 
-                self.current_port = self.current_port + 1;
+                *current_port = *current_port + 1;
                 return provider;
             }
         }
